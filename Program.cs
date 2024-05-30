29e2fa04a9730a329a60568e5d081fb02a9bde1b
nadiduno <nadiduno@gmail.com>
2024-05-29 22:36:20 -0300

    Desafio Poo Telefone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero:"8199999999", modelo: "Nokia 8", imei: "111111", o: "64")
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+console.WriteLine("\n");
+
+
+console.WriteLine("Smartphone Telegram: ");
+Smartphone iphone = new Iphone(numero:"7199999999", modelo: "iPhone 18", imei: "2222222", o: "128")
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
